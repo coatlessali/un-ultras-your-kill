51053cb1d34d4d8bcd7f6ebe0e69803a6f9ce1a5
coatlessali <coatlessali@protonmail.com>
2023-05-22 20:31:06 -0500

    Move plugin to git

---
+++ Plugin.cs
@@ -0,0 +1,59 @@
+using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace UnUltrasYourKill
+{
+	// Load plugin
+	[BepInPlugin("com.coatlessali.uuyk", "Un ULTRAs Your KILL", "1.0.0")]
+   	public class Plugin : BaseUnityPlugin
+   	{
+   		public static Plugin Instance;
+   	 	private void Awake()
+   	 	{
+   	 		Instance = this;
+           	// Plugin startup logic
+           	Logger.LogInfo("Loaded plugin.");
+           	// Honestly should I not provide binaries?
+           	Logger.LogWarning("WARNING: Your save files will be DELETED if you die.");
+           	// I have no idea what I'm doing
+           	Harmony har = new Harmony("com.coatlessali.uuyk");
+           	har.PatchAll();
+        }
+    }
+
+    // Death behavior
+	[HarmonyPatch(typeof(LaughingSkull), nameof(LaughingSkull.Start))]
+	class PatchDeath // : MonoBehaviour
+	{
+		static void Postfix()
+		{
+			int uwu = 1;
+			string path = Application.dataPath;
+			
+			// Darwin is uh, special
+    		if (Application.platform == RuntimePlatform.OSXPlayer) {
+       			path += "/../../";
+   			}
+   			else {
+      			path += "/../";
+    		}
+
+    		// You know what? Fuck you. *Un-ULTRAs your KILL* 
+			string saveDir = Path.Combine(path, "Saves");
+			while (uwu <= 5){
+				if (Directory.Exists(Path.Combine(saveDir, "Slot" + uwu)))
+				{
+					Directory.Delete(Path.Combine(saveDir, "Slot") + uwu, true);
+				}
+				uwu += 1;
+			}
+			// not actually a crash but I like calling it a crash because of how abrupt it is
+			Application.Quit();
+		}		
+	}
+}
---
+++ README.md
@@ -0,0 +1,33 @@
+
+# You Know what? Fuck you. \*Un ULTRAs Your KILL\*
+Deletes your save file and exits upon death.
+
+## Installation
+
+Copy/move `uuyk.dll` to your BepInEx plugins directory.
+
+## Uninstallation
+
+Remove `uuyk.dll` from your BepInEx plugins directory.
+
+## Usage
+
+1. Die.
+2. Save files are gone + game crashed.
+
+## Building
+
+Note: I don't use Visual Studio, so I have no clue how to compile this on Windows, though using `msbuild` should be possible. As for MacOS, the file structure is different, but by editing the .csproj file to have correct file names, you should be able to build it with `msbuild`, just like Linux, assuming you have Mono installed.\
+
+### Dependencies
+
+* A copy of ULTRAKILL with BepInEx 5.4.21 installed.
+* A stripped copy of `Assembly-CSharp.dll` from your copy of ULTRAKILL, located in `ULTRAKILL_Data/Managed/Stripped`.
+* (Linux) Mono / `msbuild`.
+
+### Instructions
+
+1. Clone the repo with git. (`git clone https://github.com/coatlessali/un-ultras-your-kill.git`)
+2. Enter the directory. (`cd un-ultras-your-kill`)
+3. Build project, with `ULTRAKILLPath` set to the path to your copy of ULTRAKILL. (`msbuild un-ultras-your-kill.csproj -p:ULTRAKILLPath=/path/to/your/ULTRAKILL/`)
+4. Copy `bin/Debug/uuyk.dll` to your BepInEx folder, if `msbuild` doesn't do it for you.
